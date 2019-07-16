6886863a4885b9fd08a73988efcf096eb580c8a4
yf12 <549626664@qq.com>
2019-07-16 21:47:40 +0800

    chunk

--- lodash/yf12-lodash.js
+++ lodash/yf12-lodash.js
@@ -1,5 +1,22 @@
-var yf12 = {
+var yf12 = {   
+    chunk: function(ary,size = 1) {
+        if(size == 0) return []
+        var result = []
+        let l = (ary.length / size) === (Math.floor(ary.length / size)) ? ary.length / size : Math.floor(ary.length / size) + 1
+        let j = 0
+        for(let i = 0;i < l;i++) {
+            var array = [],count = 0
+            while(count < size && ary[j]) {
+                array.push(ary[j])
+                j++
+                count++
+            }
+            result.push(array)
+        }
+        return result
+    },
+
     compact: function(ary) {
         return ary.filter(it => it)
-    }
+    },
 }
